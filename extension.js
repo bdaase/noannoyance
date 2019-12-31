4eed6d5c2276b1ab40378db0ac341ece9d02ce0c
Björn Daase <bjoern@daase.net>
2019-12-31 16:50:11 +0100

    Rewrite extension to allow clean disabling and reenabling
    Rewrite the extension to make sure that we keep track of the original
    WindowAttentionHandler and the signals it used, that we can properly
    disable the StealMyFocus extension and reenable the original
    WindowAttentionHandler

--- extension.js
+++ extension.js
@@ -1,35 +1,47 @@
 const Main = imports.ui.main;
-const WindowAttentionHandler = imports.ui.windowAttentionHandler;
 const Shell = imports.gi.Shell;
+const Lang = imports.lang;
 
 class StealMyFocus {
   constructor() {
-    this._tracker = Shell.WindowTracker.get_default();
-    log("Disabling 'Window Is Ready' notification");
-    global.display.disconnect(Main.windowAttentionHandler._windowDemandsAttentionId);
-    this._handlerid = global.display.connect('window-demands-attention', this._onWindowDemandsAttention.bind(this));
+    this._windowDemandsAttentionId = global.display.connect('window-demands-attention', this._onWindowDemandsAttention.bind(this));
+    this._windowMarkedUrgentId = global.display.connect('window-marked-urgent', this._onWindowDemandsAttention.bind(this));
+    log('Disabling Window Is Ready Notification');
   }
 
   _onWindowDemandsAttention(display, window) {
+    if (!window || window.has_focus() || window.is_skip_taskbar())
+            return;
+            
     Main.activateWindow(window);
   }
 
   destroy() {
-    log("Reenabling 'Window Is Ready' notification");
-    global.display.disconnect(this._handlerid);
-    global.display.connect('window-demands-attention', Main.windowAttentionHandler._onWindowDemandsAttention.bind(Main.windowAttentionHandler));
+    global.display.disconnect(this._windowDemandsAttentionId);
+    global.display.disconnect(this._windowMarkedUrgentId);
+    log('Reenabling Window Is Ready Notification');
   }
 }
 
 let stealmyfocus;
+let oldHandler;
 
 function init() {
 }
 
 function enable() {
+  global.display.disconnect(Main.windowAttentionHandler._windowDemandsAttentionId);
+  global.display.disconnect(Main.windowAttentionHandler._windowMarkedUrgentId);
+  
+  oldHandler = Main.windowAttentionHandler;
   stealmyfocus = new StealMyFocus();
+  Main.windowAttentionHandler = stealmyfocus;
 }
 
 function disable() {
   stealmyfocus.destroy();
+
+  oldHandler._windowDemandsAttentionId = global.display.connect('window-demands-attention', oldHandler._onWindowDemandsAttention.bind(oldHandler));
+  oldHandler._windowMarkedUrgentId = global.display.connect('window-marked-urgent', oldHandler._onWindowDemandsAttention.bind(oldHandler));
+  Main.windowAttentionHandler = oldHandler;
 }
